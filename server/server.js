864cf7980c478ee385b909118db4620f7140f082
Ruslan Yakushev <yakushev.rusl101@gmail.com>
2020-03-15 23:41:17 +0200

    add server side

---
+++ server/server.js
@@ -0,0 +1,14 @@
+//1. Import coingecko-api
+const CoinGecko = require('coingecko-api');
+
+//2. Initiate the CoinGecko API Client
+const CoinGeckoClient = new CoinGecko();
+
+//3. Make calls
+var func = async() => {
+  let data = await CoinGeckoClient.coins.all();
+	console.log(data["data"]);
+};
+
+
+func();
